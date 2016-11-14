355fd50944a5e797972389ab9b6298338a3de07a
MinotaroK <kyutyo.cow@gmail.com>
2016-11-14 12:41:31 +0900

    さまざまな更新

--- JS/mainController.js
+++ JS/mainController.js
@@ -1,28 +1,40 @@
 $(function(){
+
 	$("select[id=select_plan]").val("select")
 
 	$("#startBtn").click(function(e){
 		cntFlg = 1;
 		var plan = $("select[id=select_plan]").val();
+		var planName = $("select[id=select_plan] option:selected").text();
 		$("select[id=select_plan]").remove();
 		$("#startBtn").remove();
-		$("#count_sec").after("<br>" + "<button id='stopBtn'>一時停止</button>")
+		$("#count_min").before("<br>" + "<div id='selectedPlan'>戦術：" + planName + "</div>" + "<br>")
+		$("#count_sec").after("<br>" + "<br>" + "<button id='stopBtn'>一時停止</button>")
+		$("#stopBtn").after("<br>" + "<br>" + "<div id='tmpScore'></div>");
 		console.log(plan)
 		count_sec = 0;
 		count_min = 0;
 		time_score = 0;
-		tmp_score = 0;
 		timerID = setInterval(function(){ countup(plan); }, 10);
 
 		$("#stopBtn").click(function(e){
 			cntFlg = 0;
+			$("#tmpScore").remove();
 			$("#stopBtn").after("<br>" + "<button id='reStartBtn'>再開</button>")
 			$("#stopBtn").hide();
+			$("#reStartBtn").after("<br>" + "<br>" + "<div id='letSelect'>選手交代/ポジションチェンジ")
 
 			$("#reStartBtn").click(function(e){
 				cntFlg = 1;
-				$("#stopBtn").show();
+				$("#changeResult").remove();
+				$("#firstSCT").remove();
+				$("#letsendSct").remove();
+				$("#letSelect").remove();
 				$("#reStartBtn").remove();
+				$("#letSelect").remove();
+				$("#stopBtn").show();
+				$("#stopBtn").after("<br>" + "<div id='tmpScore'></div>");
+				clkList = [];
 			})
 
 		})
@@ -46,7 +58,13 @@ function countup(operation) {
 		count_sec++;
 		for(i=0; i<playerData.length; i++){
 		 	if(playerData[i].location < 12){
+		 		locate = playerData[i].location;
+		 		localObj = coordinate[locate]
+		 		if(playerData[i].position != localObj.position){
+		 			playerData[i].playtime += ((time_score/60 - playerData[i].playtime)*0.7);
+		 		}else{
 		 			playerData[i].playtime += (time_score/60 - playerData[i].playtime);
+		 		}
 		 	}
 		}
 		if(count_min > 89){
@@ -63,22 +81,14 @@ function countup(operation) {
 			$("#count_sec").html(("0" + count_sec).slice(-2));
 			for(j=1; j<10; j++){
 				if(count_min == (j + "0") && count_sec == 0){
+					tmp_score = 0;
 					for(i=0; i<playerData.length; i++){
 						tmp_score += (playerData[i].playtime * playerData[i][operation])
+						$("#tmpScore").html(j + "0分経過、現在の合計スコアは" + tmp_score);
 					}
-					console.log(j + "0分経過！");
+					console.log(j + "0分経過！現在のスコアは" + tmp_score);
 				}
 			}
 		}
 	}
 }
-
-
-
-
-
-
-
-
-
-
--- JS/viewController.js
+++ JS/viewController.js
@@ -1,20 +1,20 @@
 coordinate = {
-  "1": {"location":1,"x":220,"y":100},
-  "2": {"location":2,"x":320,"y":100},
-  "3": {"location":3,"x":130,"y":230},
-  "4": {"location":4,"x":420,"y":230},
-  "5": {"location":5,"x":320,"y":350},
-  "6": {"location":6,"x":220,"y":350},
-  "7": {"location":7,"x":420,"y":500},
-  "8": {"location":8,"x":320,"y":500},
-  "9": {"location":9,"x":220,"y":500},
-  "10": {"location":10,"x":130,"y":500},
-  "11": {"location":11,"x":275,"y":600},
-  "12": {"location":12,"x":100,"y":710},
-  "13": {"location":13,"x":190,"y":710},
-  "14": {"location":14,"x":280,"y":710},
-  "15": {"location":15,"x":370,"y":710},
-  "16": {"location":16,"x":450,"y":710}
+  "1": {"location":1, "position":"FW", "x":220,"y":100},
+  "2": {"location":2, "position":"FW", "x":320,"y":100},
+  "3": {"location":3, "position":"MF", "x":130,"y":230},
+  "4": {"location":4, "position":"MF", "x":420,"y":230},
+  "5": {"location":5, "position":"MF", "x":320,"y":350},
+  "6": {"location":6, "position":"MF", "x":220,"y":350},
+  "7": {"location":7, "position":"DF", "x":420,"y":500},
+  "8": {"location":8, "position":"DF", "x":320,"y":500},
+  "9": {"location":9, "position":"DF", "x":220,"y":500},
+  "10": {"location":10, "position":"DF", "x":130,"y":500},
+  "11": {"location":11, "position":"GK", "x":275,"y":600},
+  "12": {"location":12, "position":"bench", "x":100,"y":710},
+  "13": {"location":13, "position":"bench", "x":190,"y":710},
+  "14": {"location":14, "position":"bench", "x":280,"y":710},
+  "15": {"location":15, "position":"bench", "x":370,"y":710},
+  "16": {"location":16, "position":"bench", "x":450,"y":710}
 }
 
 const w = 500
@@ -91,11 +91,20 @@ function drawD3(){
     .attr('fill', '#0000DD')
     .on('click', function(d){
       // d はクリックした要素のデータ
+      $("#changeResult").remove();
       clkList.push(d);
+      if(clkList.length == 1){
+        console.log("working")
+        $("#letSelect").after("<br>" + "<div id='firstSCT'>選択中" + "<br>" + clkList[0]["name"] + "</div>")
+        $("#firstSCT").after("<br>" + "<div id='letsendSct'>交代先/移動先の選手を選んで下さい</div>")
+      }
       if(clkList.length > 1){
         var firstClk = clkList[0];
         var secondClk = clkList[1];
         changePlayer(firstClk, secondClk);
+        $("#firstSCT").remove();
+        $("#letsendSct").remove();
+        $("#letSelect").after("<br>" + "<div id='changeResult'>" + clkList[0]["name"] + "と" + clkList[1]["name"] + "が交代しました");
         clkList = [];
         d3.select("svg").remove();
         drawD3();
